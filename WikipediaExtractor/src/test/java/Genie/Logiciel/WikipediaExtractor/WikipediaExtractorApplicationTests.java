0b1c4f149f310efade2ec5be360376fd867ce22d
Ahmed Hammeda <ahmedhammeda19@gmail.com>
2022-03-18 18:08:15 +0100

    Partie Java avec SpringBoot (reste tabledata à modifier)

---
+++ WikipediaExtractor/src/test/java/Genie/Logiciel/WikipediaExtractor/WikipediaExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package Genie.Logiciel.WikipediaExtractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikipediaExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
